193d58a7ab8e61738d0add2307843f845452b16d
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2019-08-20 15:18:39 +0900

    ブラウザを使う関数をfmwwService.jsへ移動

---
+++ fmwwService.js
@@ -0,0 +1,142 @@
+const path = require('path');
+const {promisify} = require('util');
+const fs = require('fs');
+
+const writeFileAsync = promisify(fs.writeFile);
+
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+// 部門コード全取得
+// 戻り値：[[部門コード,部門名]]
+const fetchItems = async (page) => {
+  return await page
+    .evaluate(async () => {
+      return Array.from(document.getElementById('item_list:select').children).map(a => a.textContent.split(/\s+/))
+    }, {timeout: 0})
+}
+
+const download = async (page) => {
+  return await page
+    .evaluate(async () => {
+      const form = document.querySelector('form[id="form1"]');
+      const data = new FormData(form);
+      data.set('form1:execute', 'execute')
+
+      let xs={};
+      for(var pair of data.entries()) {
+        xs[pair[0]] = pair[1];
+      }
+      const params = createURLSearchParams(xs)
+      return await ff(form.action, {
+        method: 'POST',
+        credentials: 'include',
+        body: params,
+      })
+
+      function ff(action, op) {
+        return new Promise((resolve) => {
+          const req = new XMLHttpRequest();
+          req.open(op.method, action, true)
+          req.responseType = 'arraybuffer'
+          req.withCredentials = true
+
+          req.onload = function (oEvent) {
+            let arrayBuffer = req.response;
+            if (arrayBuffer) {
+              let byteArray = new Uint8Array(arrayBuffer);
+              resolve(byteArray)
+            }
+          };
+          req.send(op.body);
+        })
+      }
+
+      function createURLSearchParams(data) {
+        const params = new URLSearchParams();
+        Object.keys(data).forEach(key => params.append(key, data[key]));
+        return params;
+      }
+    }, {timeout: 0})
+}
+
+// 検索条件にマッチする商品マスタエクセルをローカル保存して、検索条件入力画面に戻す
+const downloadProductsExcel = async (page, options) => {
+  const itemCode = options.itemCode || ''
+  const barcode = options.barcode || ''
+  const prefix = itemCode ? itemCode + '_' : barcode + '_'
+  const filename = prefix + 'products.xlsx'
+
+  await page.evaluate(x => document.getElementById('item_list').value = x, itemCode)
+  await page.evaluate(x => document.getElementById('barcode').value = x, barcode)
+  await page.evaluate(x => document.getElementById('excel_button_ex').click())
+  await page.waitFor(() => !!document.querySelector('#loading'), {timeout: 0})
+  await page.waitFor(() => document.querySelector('#loading').style.display === 'none', {timeout: 0})
+  await page.screenshotIfDebug({ path: 'is_ready_to_download_' + filename + '.png' });
+
+  // ダウンロード処理
+  const uint8 = await download(page)
+  const xs = Object.keys(uint8).map(key => uint8[key])
+  const content = Buffer.from(xs)
+  // encodingをnullにして、生データのまま書き込む
+  await writeFileAsync(path.join(options.saveTo, filename), content, {encoding: null});
+
+  // 閉じるボタンをクリックして、非表示にしている検索条件入力画面を表示する
+  await page.evaluate(_ => document.querySelector('div.excelDLDiv input[name=cls]').click())
+  await sleep(500)
+}
+
+const signIn = async (page) => {
+  await  page.waitForSelector('#form1\\:client')
+  await Promise.all([
+    page.evaluate(arg => {
+      document.getElementById('form1:client').value = arg.FMWW_ACCESS_KEY_ID
+      document.getElementById('form1:person').value = arg.FMWW_USER_NAME
+      document.getElementById('form1:clpass').value = arg.FMWW_SECRET_ACCESS_KEY
+      document.getElementById('form1:pspass').value = arg.FMWW_PASSWORD
+
+      setTimeout(() => {
+        document.getElementById('form1:login').click()
+      }, 100)
+    }, { FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
+         FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
+         FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
+         FMWW_PASSWORD          : process.env.FMWW_PASSWORD }),
+    page.waitForNavigation({timeout: 60000, waitUntil: 'domcontentloaded'})
+  ])
+  console.log('signined')
+  await page.screenshotIfDebug({ path: 'signined.png' });
+}
+
+const decideMenuItem = async (page) => {
+  // 外部インターフェース -> 対HT -> 商品マスタメンテナンス -> 照会
+  await page.waitForSelector('#menu\\:0 div:nth-child(14)')
+  await page.waitForSelector('#menu\\:1 div:nth-child(1)')
+  await page.evaluate(_ => {
+    document.querySelector('#menu\\:0 div:nth-child(14)').click()
+    document.querySelector('#menu\\:1 div:nth-child(1)').click()
+  }),
+  await page.waitForSelector('#menu\\:2 div:nth-child(4) div:nth-child(3)')
+  console.log('menu')
+  await page.screenshotIfDebug({ path: 'menu.png' });
+
+  await Promise.all([
+    page.evaluate(x => {
+      document.querySelector('#menu\\:2 div:nth-child(4) div:nth-child(3)').click()
+    }),
+    page.waitForNavigation({timeout: 60000, waitUntil: 'domcontentloaded'})
+  ])
+  await page.waitFor(() => !!document.querySelector('#loading'))
+  await page.waitFor(() => document.querySelector('#loading').style.display === 'none')
+  console.log('criteria')
+  await page.screenshotIfDebug({ path: 'criteria.png' });
+}
+
+exports.fetchItems = fetchItems
+exports.download = download
+exports.downloadProductsExcel = downloadProductsExcel
+exports.signIn = signIn
+exports.decideMenuItem = decideMenuItem

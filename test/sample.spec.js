6b07b81aab6f34d94edb59198fef92cdb803707e
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2019-09-03 11:44:21 +0900

    プロジェクトに関係ないファイルを削除

--- test/sample.spec.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-
-describe('sample test', function () {
-  it('should work', async function () {
-    console.log(await browser.version());
-    expect(true).to.be.true;
-  });
-});

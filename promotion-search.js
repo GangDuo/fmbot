e9c5aa529cdac95af53fd83436dc4163b7afd7ea
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2019-10-17 13:23:57 +0900

    ポイント還元イベント関連サブコマンドsearchオプションの設定日付デフォルト値を今日の日付とする。

--- promotion-search.js
+++ promotion-search.js
@@ -1,9 +1,10 @@
+const moment = require('moment');
 const program = require('commander');
 const PromotionController = require('./src/fm-client/controllers/for-shop/customers/PromotionController')
 
 program
-.option('-b, --begin-date <date>', '設定日付 自', '今日')
-.option('-e, --end-date <date>', '設定日付 至', '今日')
+.option('-b, --begin-date <date>', '設定日付 自', moment().format('YYYY-MM-DD'))
+.option('-e, --end-date <date>', '設定日付 至', moment().format('YYYY-MM-DD'))
 .option('-n, --setting-number <value>', '設定番号', '')
 .action(PromotionController.onStart)
 .parse(process.argv)

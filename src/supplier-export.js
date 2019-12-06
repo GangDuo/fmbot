e2e6b0f8236a15082a6ed6586d2450d41d88be1f
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2019-12-06 14:32:57 +0900

    supplier export コマンド実装

--- src/supplier-export.js
+++ src/supplier-export.js
@@ -2,5 +2,6 @@ const program = require('commander');
 const SupplierController = require('./fm-client/controllers/master/others/SupplierController')
 
 program
+.option('-o, --output <file>', '標準出力ではなく<file>に出力を書き込みます。')
 .action(SupplierController.export)
 .parse(process.argv)

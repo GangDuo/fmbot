ddd8073f0d01cc9c4c569ecb3e0a22da583c672d
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-01-20 14:43:48 +0900

    コマンドライン引数で指定した会員にポイントを付与する。

--- src/points-create.js
+++ src/points-create.js
@@ -2,5 +2,9 @@ const program = require('commander');
 const PointsController = require('./fm-client/controllers/for-shop/customers/PointsController')
 
 program
+.option('-m, --membership-number <number>', '会員番号')
+.option('-s, --store-code <code>', 'ポイント発行店舗コード')
+.option('-o, --owner <code>', '入力担当者コード', 9900)
+.option('-p, --points <value>', '発行ポイント')
 .action(PointsController.create)
 .parse(process.argv)

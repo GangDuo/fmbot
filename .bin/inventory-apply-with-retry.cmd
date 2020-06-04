77b0a918f5900a4a5b8b9fd10e1daf73d57b38f0
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-06-04 11:09:16 +0900

    在庫・棚卸 -> 棚卸更新 -> 実行に失敗したら、成功するまで再試行する。

---
+++ .bin/inventory-apply-with-retry.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+:apply
+npm run inventory -- apply %*
+
+if not %ERRORLEVEL% == 0 (
+  echo ERROR: Failed to apply inventory
+  timeout 60 /nobreak
+  goto apply
+)
+
+echo Successed to apply inventory
+exit /b 0
--- src/fm-client/controllers/inventory/issuance/InventoryScheduleController.js
+++ src/fm-client/controllers/inventory/issuance/InventoryScheduleController.js
@@ -2,26 +2,34 @@ const {FmClient, Schedule} = require('fmww-library');
 
 module.exports = class InventoryScheduleController {
   static async create(options) {
-    const c = new FmClient()
-    const ability = await c
-      .open(process.env.FMWW_SIGN_IN_URL)
-      .signIn({
-        FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
-        FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
-        FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
-        FMWW_PASSWORD          : process.env.FMWW_PASSWORD
-      })
-      .createAbility(Schedule)
-    const response = await c.create({
-      stocktakingDate: options.stocktakingDate || '',
-      storeCodes: options.storeCodes || [],
-      zeroFill: options.zeroFill || false
+    await create(options).catch(e => {
+      process.stderr.write(`${e.message}\n`)
+      process.exitCode = 1;
     })
-    if(response.isSuccess) {
-      process.stdout.write(`${response.statusText}\n`)
-    } else {
-      process.stderr.write(response.statusText)
-    }
-    await c.quit()
+  }
+}
+
+async function create(options) {
+  const c = new FmClient()
+  const ability = await c
+    .open(process.env.FMWW_SIGN_IN_URL)
+    .signIn({
+      FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
+      FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
+      FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
+      FMWW_PASSWORD          : process.env.FMWW_PASSWORD
+    })
+    .createAbility(Schedule)
+  const response = await c.create({
+    stocktakingDate: options.stocktakingDate || '',
+    storeCodes: options.storeCodes || [],
+    zeroFill: options.zeroFill || false
+  })
+  await c.quit()
+
+  if(response.isSuccess) {
+    process.stdout.write(`${response.statusText}\n`)
+  } else {
+    throw new Error(response.statusText);
   }
 }

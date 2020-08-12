8b5b6e345dc69b735c5e801bd69ed0e29f76ac67
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-08-12 13:26:10 +0900

    標準入力から会員番号、店舗コード、加算ポイントを読み込み、同じ1つのブラウザインスタンスを使って連続でポイント加算する

--- src/fm-client/controllers/for-shop/customers/PointsController.js
+++ src/fm-client/controllers/for-shop/customers/PointsController.js
@@ -1,4 +1,5 @@
 const {FmClient, Points} = require('fmww-library');
+const readline = require('readline');
 
 module.exports = class PointsController {
   static search(options) {
@@ -7,7 +8,7 @@ module.exports = class PointsController {
 
   static async create(options) {
     const c = new FmClient()
-    const response = await c
+    await c
       .open(process.env.FMWW_SIGN_IN_URL)
       .signIn({
         FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
@@ -16,14 +17,58 @@ module.exports = class PointsController {
         FMWW_PASSWORD          : process.env.FMWW_PASSWORD
       })
       .createAbility(Points)
-      .create({
-        membershipNumber: options.membershipNumber,
-        storeCode: options.storeCode,
-        owner: options.owner,
-        points: options.points,
-        grounds: '04'
-      })
-    await c.quit()
-    console.log(response.isSuccess ? 'Success' : response.statusText)
+
+    try {
+      if(options.membershipNumber &&
+         options.storeCode &&
+         options.points) {
+        const response = await create(c, options)
+        console.log(response.isSuccess ? 'Success' : response.statusText)
+        return
+      }
+      console.log('Read from stdin...')
+      const getLine = useReadLine()
+      for (;;) {
+        const line = await getLine()
+        if(!line) {
+          return;
+        }
+        const values = line.split(',')
+        const response = await create(c, {
+          membershipNumber: values[0],
+          storeCode: values[1],
+          owner: options.owner,
+          points: values[2]
+        })
+        console.log(`${line}: ` + (response.isSuccess ? 'Success' : response.statusText))
+      };
+    } catch(e) {
+    } finally {
+      await c.quit()
+    }
   }
+}
+
+async function create(client, options) {
+  const response = await client
+    .create({
+      membershipNumber: options.membershipNumber,
+      storeCode: options.storeCode,
+      owner: options.owner,
+      points: options.points,
+      grounds: '04'
+    })
+  return response
+}
+
+function useReadLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+  });
+  const getLineGen = (async function* () {
+    for await (const line of rl) {
+      yield line;
+    }
+  })();
+  return async () => ((await getLineGen.next()).value);
 }

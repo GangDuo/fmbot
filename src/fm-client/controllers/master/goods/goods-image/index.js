68da3027843da66187186f0be3a42f1a1e7db0dd
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-04-06 13:19:38 +0900

    標準入力から1行ずつ読み込みした結果を処理する #4

--- src/fm-client/controllers/master/goods/goods-image/index.js
+++ src/fm-client/controllers/master/goods/goods-image/index.js
@@ -1,5 +1,21 @@
+const readline = require('readline');
+
 module.exports = class GoodsImageController {
-  static export(options) {
-    console.log('goods-image-export')
+  static export(others, options) {
+    console.log("goods-image-export");
+    if (others.length > 0) {
+      // ファイルドロップ
+    } else {
+      // 標準入力
+      readline.createInterface({
+        input: process.stdin
+      })
+      .on('line', (line) => {
+        console.log(`model number is %s`, line)
+      })
+      .on('close', () => {
+        console.log("END!");
+      });      
+    }
   }
-}
+};

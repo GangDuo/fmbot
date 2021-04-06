aa40901dc45d82bec4b1e66ddf562d04e9489f9a
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-04-06 15:44:22 +0900

    ファイルドロップ時の処理 #4

--- src/fm-client/controllers/master/goods/goods-image/index.js
+++ src/fm-client/controllers/master/goods/goods-image/index.js
@@ -1,10 +1,12 @@
 const readline = require('readline');
 
 module.exports = class GoodsImageController {
-  static export(others, options) {
+  static async export(others, options) {
     console.log("goods-image-export");
     if (others.length > 0) {
       // ファイルドロップ
+      const result = await GoodsImageController.handleDragDrop_(others).catch(e => e)
+      console.log(result)
     } else {
       // 標準入力
       readline.createInterface({
@@ -18,4 +20,26 @@ module.exports = class GoodsImageController {
       });      
     }
   }
+
+  static handleDragDrop_ = (array) => new Promise((resolve, reject) => {
+    const next = (xs) => {
+      const x = xs.shift();
+      if(x) {
+        try {
+          // do something
+          console.log(x)
+
+          setImmediate(() => {
+            next(xs)
+          });
+        } catch (error) {
+          reject(error)
+        }
+      } else {
+        resolve({status: "OK"})
+      }
+    }
+    
+    next(array)
+  })
 };

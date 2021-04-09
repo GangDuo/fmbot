3330ba6f66e9c86a78b3cb03bd6d4d136a8f285c
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-04-09 11:25:17 +0900

    インスタンスメソッドに変更する #4

--- src/fm-client/controllers/master/goods/goods-image/index.js
+++ src/fm-client/controllers/master/goods/goods-image/index.js
@@ -5,12 +5,12 @@ const {FmClient, GoodsImage} = require('fmww-library');
 module.exports = class GoodsImageController {
   static async export(others, options) {
     console.log("goods-image-export");
+    const instance = new GoodsImageController()
     if (others.length > 0) {
       // ファイルドロップ
-      const result = await GoodsImageController.handleDragDrop_(others).catch(e => e)
+      const result = await instance.handleDragDrop_(others).catch(e => e)
       console.log(result)      
     } else {
-      const instance = new GoodsImageController()
       // 標準入力
       readline.createInterface({
         input: process.stdin
@@ -42,32 +42,33 @@ module.exports = class GoodsImageController {
       .quit()
   }
 
-  static handleDragDrop_ = (array) => new Promise((resolve, reject) => {
-    const next = (xs) => {
-      const x = xs.shift();
-      if(x) {
-        try {
-          const instance = new GoodsImageController()
-          // do something
-          console.log(x)
-          readline.createInterface({
-            input: fs.createReadStream(x)
-          })
-          .on('line', instance.handleReadLine_.bind(instance))
-          .on('close', () => {
-            console.log("END!");
-            setImmediate(() => {
-              next(xs)
-            });
-          });     
-        } catch (error) {
-          reject(error)
+  handleDragDrop_(array) {
+    return new Promise((resolve, reject) => {
+      const next = (xs) => {
+        const x = xs.shift();
+        if(x) {
+          try {
+            // do something
+            console.log(x)
+            readline.createInterface({
+              input: fs.createReadStream(x)
+            })
+            .on('line', this.handleReadLine_.bind(this))
+            .on('close', () => {
+              console.log("END!");
+              setImmediate(() => {
+                next(xs)
+              });
+            });     
+          } catch (error) {
+            reject(error)
+          }
+        } else {
+          resolve({status: "OK"})
         }
-      } else {
-        resolve({status: "OK"})
       }
-    }
-    
-    next(array)
-  })
+      
+      next(array)
+    })
+  }
 };

015d0cd7177f235341d251a22b799443d5138322
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-04-09 15:10:45 +0900

    1つのFmClientインスタンスを再利用する #4

--- src/fm-client/controllers/master/goods/goods-image/index.js
+++ src/fm-client/controllers/master/goods/goods-image/index.js
@@ -6,28 +6,25 @@ module.exports = class GoodsImageController {
   static async export(others, options) {
     console.log("goods-image-export");
     const instance = new GoodsImageController()
+    await instance.setUp()
+
     if (others.length > 0) {
       // ファイルドロップ
       const result = await instance.handleDragDrop_(others).catch(e => e)
       console.log(result)      
     } else {
       // 標準入力
-      readline.createInterface({
-        input: process.stdin
-      })
-      .on('line', instance.handleReadLine_.bind(instance))
-      .on('close', () => {
-        console.log("END!");
-      });      
+      await instance.handleStdin_()
     }
+    await instance.tearDown()
   }
 
-  async handleReadLine_(line) {
-    if(line.length === 0) return
-    console.log(`model number is %s`, line)
+  constructor() {
+    this.fmClient = new FmClient()
+  }
 
-    await new FmClient()
-      .open(process.env.FMWW_SIGN_IN_URL)
+  async setUp() {
+    await this.fmClient.open(process.env.FMWW_SIGN_IN_URL)
       .signIn({
         FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
         FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
@@ -35,31 +32,56 @@ module.exports = class GoodsImageController {
         FMWW_PASSWORD          : process.env.FMWW_PASSWORD
       })
       .createAbility(GoodsImage)
-      .export({
-        baseURL: process.env.FMWW_SIGN_IN_URL,
-        modelNumber: line
-      })
-      .quit()
+  }
+
+  async tearDown() {
+    await this.fmClient.quit()
+  }
+
+  async handleReadLine_(line) {
+    if(line.length === 0) return
+    console.log(`model number is %s`, line)
+
+    // 1つのFmClientインスタンスを再利用する
+    // FmClientはブラウザインスタンスを生成するため、
+    // 大量の画像をダウンロードする時に
+    // ここでnew FmClientすると画像枚数分のブラウザインスタンスを生成しようとして
+    // リソース上限に達し失敗する
+    await this.fmClient.export({
+      baseURL: process.env.FMWW_SIGN_IN_URL,
+      modelNumber: line
+    })
+
+    console.log(`downloaded %s`, line)
+  }
+
+  async handleStdin_() {
+    const rl = readline.createInterface({
+      input: process.stdin
+    })
+
+    for await (const line of rl) {
+      await this.handleReadLine_(line)
+    }
+    console.log("END!");
   }
 
   handleDragDrop_(array) {
-    return new Promise((resolve, reject) => {
-      const next = (xs) => {
+    return new Promise(async(resolve, reject) => {
+      const next = async (xs) => {
         const x = xs.shift();
         if(x) {
           try {
             // do something
             console.log(x)
-            readline.createInterface({
+            const rl = readline.createInterface({
               input: fs.createReadStream(x)
             })
-            .on('line', this.handleReadLine_.bind(this))
-            .on('close', () => {
-              console.log("END!");
-              setImmediate(() => {
-                next(xs)
-              });
-            });     
+            for await (const line of rl) {
+              await this.handleReadLine_(line)
+            }
+            console.log("END!");
+            await next(xs)
           } catch (error) {
             reject(error)
           }
@@ -68,7 +90,7 @@ module.exports = class GoodsImageController {
         }
       }
       
-      next(array)
+      await next(array)
     })
   }
 };
